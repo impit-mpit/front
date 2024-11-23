8f41bf2af588948d70d40a0a395763957682040d
hmnneedhelp <ilovememesdontkillme@gmail.com>
2024-11-23 18:54:13 +0900

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,17 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import TanStackRouterRspack from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
-  plugins: [pluginReact(),],
+  plugins: [pluginReact(),
+    {
+      setup: (api) => {
+        api.onAfterBuild(({ isFirstCompile, stats }) => {
+          if (typeof Bun !== 'undefined') {
+            process.exit(process.exitCode || 0);            
+          }
+        });
+      },
+    },
+  ],
   html: {
     template: "./index.html"
   },
